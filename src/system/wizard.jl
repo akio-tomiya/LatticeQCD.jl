0e76a6a5811f10cd5906e7a59fe3183faa2f9f9d
cometscome <cometscome@gmail.com>
2021-01-27 18:03:17 +0900

    thermalization is added

--- src/system/wizard.jl
+++ src/system/wizard.jl
@@ -365,7 +365,7 @@ module Wizard
                 
 
                 if system["update_method"] == "HMC" || system["update_method"] == "IntegratedHMC" || system["update_method"] == "SLHMC"|| system["update_method"] == "Heatbath" || system["update_method"] == "SLMC"
-                    Nthermalization = parse(Int64,Base.prompt("Input number of thermalization steps", default="10"))
+                    Nthermalization = parse(Int64,Base.prompt("Input number of thermalization steps", default="0"))
                     Nsteps = parse(Int64,Base.prompt("Input number of total trajectories", default="100"))
 
                     if Nthermalization<0
